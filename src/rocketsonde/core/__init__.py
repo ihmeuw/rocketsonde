34fde00b1f6933a69b8971849de4e8873fcfd16b
Alec Deason <alecwd@uw.edu>
2019-02-01 11:56:15 -0800

    Move summarizer up to the top level

--- src/rocketsonde/core/__init__.py
+++ src/rocketsonde/core/__init__.py
@@ -1,4 +1,5 @@
 from .probe import Probe
 from .metrics import basic_metric
+from .summarize import basic_summarizer
 
-__all__ = ["Probe", "basic_metric"]
+__all__ = ["Probe", "basic_metric", "basic_summarizer"]

c2553ae5157499980a7a7b2211fa737b204257fc
Alec Deason <alecwd@uw.edu>
2019-02-21 16:10:47 -0800

    Move to a lower privilege metric

--- src/rocketsonde/core/metrics.py
+++ src/rocketsonde/core/metrics.py
@@ -4,9 +4,9 @@ import psutil
 def basic_metric(pid):
     try:
         p = psutil.Process(pid)
-        rss = p.memory_full_info().rss
-        uss = p.memory_full_info().uss
+        rss = p.memory_info().rss
+        shared = p.memory_info().shared
         cpu = p.cpu_times()
-        return {"rss": rss, "uss": uss, "user": cpu.user, "system": cpu.system}
+        return {"rss": rss, "shared": shared, "user": cpu.user, "system": cpu.system}
     except psutil.NoSuchProcess:
         return None
